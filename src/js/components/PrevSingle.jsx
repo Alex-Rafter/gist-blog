ff6d05f281256ec2b5c455eb5065103df89ecc80
Alex Rafter <alexrafteremail@gmail.com>
2022-05-30 22:12:27 +0100

    updated to use useRef to fetch only once on mouse over

--- src/js/components/PrevSingle.jsx
+++ src/js/components/PrevSingle.jsx
@@ -1,14 +1,36 @@
 import { h, render, Component, createContext } from "preact";
-import { useContext, useEffect, useState } from "preact/hooks";
+import { useContext, useEffect, useState, useRef } from "preact/hooks";
 import { parse } from "preact-parser";
 
 // main();
 
 
-export function PrevSingle({createdAt, description, index}) {
+export function PrevSingle({ createdAt, description, url, id, index }) {
+  const [data, setData] = useState([]);
   const x = String(description).replace(/\s/g, '-').replace(/\-#.*/, '')
+  const [fetched, setFetched] = useState(false);
+  const previousFetchedValue = useRef("");
+
+  useEffect(() => {
+    previousFetchedValue.current = fetched;
+  }, [fetched]);
+
+
+  async function getBlogItem() {
+    if (fetched === true) return
+    setFetched(true)
+    let url =
+      `https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/gistsToSheets/gistBlog/${id}`;
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("Network response was not OK");
+    const json = await response.json();
+    const Data = await json.gistBlog;
+    setData(Data)
+    console.log(Data)
+  }
+
   return (
-    <div className="card mb-4">
+    <div className="card mb-4" onMouseOver={getBlogItem}>
       <div className="card-body">
         <div className="small text-muted">{createdAt}</div>
         <h2 className="card-title">{description}</h2>
--- src/js/pages/Article.jsx
+++ src/js/pages/Article.jsx
@@ -8,20 +8,20 @@ export function Article({ id }) {
     const [rootDisplay, setRootDisplay] = useState('d-none')
     const [data, setData] = useState([]);
 
-    useEffect(() => {
-        getData();
-    }, []);
+    // useEffect(() => {
+    //     getData();
+    // }, []);
 
-    async function getData() {
-        let url =
-            `https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/gistsToSheets/gistBlog/${id}`;
-        const response = await fetch(url);
-        if (!response.ok) throw new Error("Network response was not OK");
-        const json = await response.json();
-        const Data = await json.gistBlog;
-        setData(Data)
-        setRootDisplay('');
-    }
+    // async function getData() {
+    //     let url =
+    //         `https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/gistsToSheets/gistBlog/${id}`;
+    //     const response = await fetch(url);
+    //     if (!response.ok) throw new Error("Network response was not OK");
+    //     const json = await response.json();
+    //     const Data = await json.gistBlog;
+    //     setData(Data)
+    //     setRootDisplay('');
+    // }
 
     return (
         <div class={`container-fluid px-0 overflow-hidden ${rootDisplay}`}>
