9c3e5dc67a5a79c96a6b2830b540c65ac73326da
Alex Rafter <alexrafteremail@gmail.com>
2022-06-04 11:26:45 +0100

    refactor index

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,48 +1,48 @@
+// Libs
 import { Router } from 'preact-router';
-import { h, render, Component, createContext } from "preact";
-import { useContext, useEffect, useState } from "preact/hooks";
-import { Nav } from "./components/Nav"
-import { Footer } from "./components/Footer"
-import { Previews } from './components/Previews';
-import { Article } from "./pages/Article";
+import { h, render, createContext } from "preact";
+import { useEffect, useState } from "preact/hooks";
 import { createHashHistory } from 'history';
 
+// Components
+import { Previews } from './components/Previews';
+import { Article } from "./pages/Article";
 
-/** @jsx h */
+// Global State
 export const BlogContext = createContext()
 
+// Component
 const Main = () => {
-    const [rootDisplay, setRootDisplay] = useState('d-none')
-    const [data, setData] = useState([]);
+    const [previewData, setPreviewData] = useState([]);
 
     useEffect(() => {
-        getData();
+        getPreviewData();
     }, []);
 
-    if (data.length > 0) setRootDisplay('');
-
-    async function getData() {
+    async function getPreviewData() {
         let url =
             "https://api.sheety.co/7016cabf6b37601c93f0bcbd5ec85980/gistsToSheets/gistBlog";
         const response = await fetch(url);
         if (!response.ok) throw new Error("Network response was not OK");
         const json = await response.json();
-        const Data = await json.gistBlog;
-        console.log(Data)
-        setData(Data)
+        const gistBlogData = await json.gistBlog;
+        setPreviewData(gistBlogData)
     }
-    const x = item => String(item.description).replace(/\s/g, '-').replace(/\-#.*/, '')
+
+    const rmHashTagsAndSlugify = description => String(description).replace(/\s/g, '-').replace(/\-#.*/, '')
+
     return (
-        <div class={`${rootDisplay}`}>
-            <BlogContext.Provider value={data}>
+        <div class={(previewData.length > 0) ? '' : 'd-none'}>
+            <BlogContext.Provider value={previewData}>
                 <Router history={createHashHistory()}>
-                    <Previews path="/" data={data} />
-                    {data.map((article) => <Article path={`/${x(article)}`} {...article} />)}
+                    <Previews path="/" data={previewData} />
+                    {previewData.map(article => <Article path={`/${rmHashTagsAndSlugify(article.description)}`} {...article} />)}
                 </Router>
             </BlogContext.Provider>
         </div>
     )
 }
 
+// Render
 const root = document.querySelector("#root");
 render(<Main />, root);
