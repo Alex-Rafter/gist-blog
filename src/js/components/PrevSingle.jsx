7d869f13b6fc437fb0bd981abf6112b4e0bf8335
Alex Rafter <alexrafteremail@gmail.com>
2022-06-07 10:12:35 +0100

    updated article list styles

--- src/js/components/PrevSingle.jsx
+++ src/js/components/PrevSingle.jsx
@@ -14,7 +14,7 @@ export function PrevSingle({ createdAt, description, id }) {
   const [fetched, setFetched] = useState(false);
   const previousFetchedValue = useRef("");
   const descEdited = String(description).replace(/#.*$/,'')
-  const dateEdited = String(createdAt).replace(/T.*$/,'')
+  const dateEdited = String(createdAt).replace(/T.*$/,'').replace(/-/g, '')
 
   useEffect(() => {
     previousFetchedValue.current = fetched;
@@ -25,18 +25,14 @@ export function PrevSingle({ createdAt, description, id }) {
     setFetched(true)
     const Data = await jsonFromSheets(`${apiUrl}${id}`)
     sessionStorage.setItem("article", JSON.stringify(Data))
-    console.log(e.target)
-    e.target.closest('div.preview').classList.toggle('filter')
-    const body = document.querySelector('#body')
-    body.classList.add('blurry')
   }
 
   return (
-    <div className="preview mb-4" onMouseOver={(e) => getBlogItem(e)}>
-        <a className="text-decoration-none" href={`/${rmHashTagsAndSlugify(description)}/`}>
-          <h2 className="h1 fw-light text-dark mb-1 text-lowercase font-monospace">{descEdited}</h2>
+    <div className="preview mb-4 position-relative" onMouseOver={(e) => getBlogItem(e)}>
+        <a className="preview__anchor text-decoration-none" href={`/${rmHashTagsAndSlugify(description)}/`}>
+          <h2 className="h3 fw-light text-dark mb-1 text-lowercase font-monospace">{descEdited}</h2>
+          <p class="small text-danger h-100 d-flex align-items-center mb-0 position-absolute end-0 top-0">{dateEdited}</p>          
         </a>
-        <p className="small text-muted">{dateEdited}</p>
     </div>
   );
 }
